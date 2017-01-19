c073b091d75e8042056295a793f6896b47c7269c
dannylui91 <dannylui91@gmail.com>
2017-01-18 19:34:47 -0500

    Added viewpager with tabs and empty fragments

--- app/src/main/java/com/example/andresarango/memeit/MainActivity.java
+++ app/src/main/java/com/example/andresarango/memeit/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.andresarango.memeit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.andresarango.memeit.tabfragments.HomeFragment;
+import com.example.andresarango.memeit.tabfragments.MemeListFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tablayout; // Import design in build.gradle
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tablayout = (TabLayout) findViewById(R.id.tab_layout);
+        pager = (ViewPager) findViewById(R.id.viewpager);
+
+        setupToolbar(toolbar);
+        setupTabLayout(tablayout);
+        setupViewPager(pager);
+        tablayout.setupWithViewPager(pager);
+    }
+
+    private void setupToolbar(Toolbar toolbar) {
+        toolbar.setTitle("Placeholder Title");
+        toolbar.setTitleTextColor(0xFFFFFFFF);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupTabLayout(TabLayout tablayout) {
+        tablayout.setTabTextColors(0xFFFFFFFF, 0xFFFFFFFF);
+        tablayout.setSelectedTabIndicatorColor(0xFFFFFFFF);
+    }
+
+    private void setupViewPager(ViewPager pager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new MemeListFragment(), "Meme List");
+        pager.setAdapter(adapter);
     }
 }
