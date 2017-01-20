ef87b00b8b7df745b1ef85ae90ae7daeb3d77a0b
Leigh Douglas <leighcdouglas1@gmail.com>
2017-01-20 00:59:13 -0500

    added recyclerview and image view to edit meme activity

--- app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
+++ app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
@@ -2,12 +2,24 @@ package com.example.andresarango.memeit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 public class EditMemeActivity extends AppCompatActivity {
+
+    private RecyclerView memeRv;
+    private ImageView memeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_meme);
+
+        memeImage = (ImageView) findViewById(R.id.meme_image);
+
+        memeRv = (RecyclerView) findViewById(R.id.meme_rv);
+        memeRv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
+
 }
