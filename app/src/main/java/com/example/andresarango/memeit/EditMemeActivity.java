a2ce1d4c0e3db457dbf4c62f1d8c65031d42f948
cito125 <cito125@gmail.com>
2017-01-20 19:31:16 -0500

    added image editing library

--- app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
+++ app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
@@ -1,14 +1,18 @@
 package com.example.andresarango.memeit;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.adobe.creativesdk.aviary.AdobeImageIntent;
 
 public class EditMemeActivity extends AppCompatActivity {
 
@@ -21,6 +25,35 @@ public class EditMemeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_meme);
         initialize();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    /* 1) Make a new Uri object (Replace this with a real image on your device) */
+        Uri imageUri = Uri.parse("content://media/external/images/media/####");
+
+    /* 2) Create a new Intent */
+        Intent imageEditorIntent = new AdobeImageIntent.Builder(this)
+                .setData(imageUri)
+                .build();
+
+    /* 3) Start the Image Editor with request code 1 */
+        startActivityForResult(imageEditorIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+
+                /* 4) Make a case for the request code we passed to startActivityForResult() */
+                case 1:
+                    /* 5) Show the image! */
+                    Uri editedImageUri = data.getParcelableExtra(AdobeImageIntent.EXTRA_OUTPUT_URI);
+                    memeImage.setImageURI(null);
+                    memeImage.setImageURI(editedImageUri);
+                    break;
+            }
+        }
     }
 
     private void initialize() {
