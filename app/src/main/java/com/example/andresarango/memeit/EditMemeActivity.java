aa4c8866aa16b6de4fae31d45609c14d92219edc
Helen Chan <hchan22@gmail.com>
2017-01-21 18:05:32 -0500

    camera logic

--- app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
+++ app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
@@ -2,13 +2,13 @@ package com.example.andresarango.memeit;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -101,5 +101,13 @@ public class EditMemeActivity extends AppCompatActivity implements EditorViewHol
     @Override
     public void swapFragment(Fragment memeFragment) {
         startFragment(null, memeFragment);
+    }
+
+    private void showPicture() {
+        Bitmap picture = getIntent().getParcelableExtra("BitmapCamera");
+        Uri pictureUri = Uri.parse(getIntent().getStringExtra("CameraPhotoUri"));
+        if (picture != null) {
+            memeImage.setImageBitmap(picture);
+        }
     }
 }
