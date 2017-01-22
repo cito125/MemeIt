7283c3310a28ce0e04eaa8b1109a9747764983ed
CurrentlyJordan <jwsmith451@gmail.com>
2017-01-22 13:25:40 -0500

    fixed stock image crash

--- app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
+++ app/src/main/java/com/example/andresarango/memeit/EditMemeActivity.java
@@ -55,7 +55,6 @@ public class EditMemeActivity extends AppCompatActivity implements EditorViewHol
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_meme);
 
-//        mMemeImageBitmap = getBitmapFromUri(getIntent().getStringExtra("ImageString"));
 
         initialize(savedInstanceState);
 
